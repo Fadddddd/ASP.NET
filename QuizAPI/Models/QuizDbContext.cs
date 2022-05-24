72e379656712609595231a238812cce2ca34bdaf
fadoua <fadelhannachi@gmail.com>
2022-05-24 17:22:19 +0200

    asp net first commit

---
+++ QuizAPI/Models/QuizDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizAPI.Models
+{
+    public class QuizDbContext: DbContext
+    {
+     public QuizDbContext(DbContextOptions<QuizDbContext>options):base(options)
+        {
+
+        }
+    }
+}
